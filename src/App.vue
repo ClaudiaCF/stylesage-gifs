5bf9dbe1964073c745b3c7e48c6953791bb79f13
ClaudiaCF <clau.coffee@gmail.com>
2022-03-20 19:00:06 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,128 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="app">
+    <header class="app__header">
+      <div class="app__header-texts-wrapper">
+        <span class="app__text app__text--header app__text--company">StyleSage</span>
+        <span class="app__text app__text--header app__text--welcome">Welcome John</span>
+      </div>
+    </header>
+    <div class="app__content">
+      <h1 class="app__title">Gatetes</h1>
+      <section class="app__gifs">
+        <card class="app__card" v-for="gif in gifs" :key="gif.title" :gif="gif" :style="gifCardStyle" />
+      </section>
+    </div>
+    <footer class="app__footer">
+      <span class="app__text app__text--footer">Claudia CF 2022</span>
+    </footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import gifsJson from './assets/gifs.json'
+import Card from './components/Card'
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  name: 'App',
+  components: { Card },
+  computed: {
+    gifCardStyle: function () {
+      return {
+        flexBasis: `calc((100% - ${this.cardMargin * 2 * this.cardsPerRow}px) / ${this.cardsPerRow})`,
+        margin: `${this.cardMargin}px`
+      }
+    },
+    cardsPerRow() {
+      const mobileWidth = 480
+      const tabletWidth = 768
+      if (this.windowWidth <= mobileWidth) {
+        return 1
+      } else if (this.windowWidth <= tabletWidth) {
+        return 2
+      }
+      return 4
+    }
   },
-};
+  data: function () {
+    return {
+      gifs: gifsJson,
+      cardMargin: 10,
+      windowWidth: window.innerWidth
+    }
+  },
+  methods: {
+    changeWindowWidth() {
+      this.windowWidth = window.innerWidth
+    }
+  },
+  created() {
+    window.addEventListener('resize', this.changeWindowWidth)
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.changeWindowWidth)
+  }
+}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+//I usually follow this https://css-tricks.com/poll-results-how-do-you-order-your-css-properties/
+.app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  &__header,
+  &__footer {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 40px;
+    background-color: darkgrey;
+  }
+
+  &__header-texts-wrapper {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    padding: 0 10px;
+  }
+
+  &__content {
+    flex: 1;
+    overflow-y: scroll;
+    height: 100%;
+    padding: 0 10px;
+  }
+
+  &__gifs {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 -10px;
+  }
+
+  &__text {
+    &--footer {
+      padding-left: 10px;
+    }
+  }
+
+  &__button {
+    grid-area: button;
+    align-self: center;
+    justify-self: end;
+    padding: 10px 20px;
+    border-radius: 25px;
+    border: none;
+    margin-right: 10px;
+    outline: none;
+    color: white;
+    background-color: slateblue;
+    cursor: pointer;
+  }
+
+  &__footer {
+    left: 0;
+    bottom: 0;
+  }
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,76 @@
+<template>
+  <article class="card">
+    <figure class="card__image-wrapper">
+      <img class="card__image" :src="gif.gifurl" :alt="gif.title" @error="onResultImageError" />
+    </figure>
+    <p class="card__text card__text--title">
+      {{ gif.title }}
+    </p>
+    <button class="card__button">Go</button>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    gif: Object
+  },
+  methods: {
+    onResultImageError: function (event) {
+      event.target.src = 'https://c.tenor.com/0r7ZDoQe-moAAAAd/creepy-cat-cat-smile.gif'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.card {
+  position: relative;
+  display: grid;
+  grid-template-rows: 220px 70px 70px;
+  grid-template-areas: 'image' 'name' 'button';
+  background-color: lightsteelblue;
+
+  &__image-wrapper {
+    grid-area: image;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0;
+    background-color: lightgrey;
+  }
+
+  &__image {
+    max-width: 100%;
+    max-height: 100%;
+    object-fit: contain;
+  }
+
+  &__text {
+    display: -webkit-box;
+    overflow: hidden;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+
+    &--title {
+      grid-area: name;
+      padding: 0 10px;
+    }
+  }
+
+  &__button {
+    grid-area: button;
+    align-self: center;
+    justify-self: end;
+    padding: 10px 20px;
+    border-radius: 25px;
+    border: none;
+    margin-right: 10px;
+    outline: none;
+    color: white;
+    background-color: mediumslateblue;
+    cursor: pointer;
+  }
+}
+</style>
